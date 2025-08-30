edcac67a5b79a8b81a5dbdd71f9955e0fbe90551
Eddy12597 <eddy12597@163.com>
2025-08-30 09:13:04 +0800

    added clauses as argument

--- src/resolution.py
+++ src/resolution.py
@@ -3,14 +3,16 @@ from operationals import *
 import document as doc
 
 class Resolution:
-    def __init__(self, committee: str = "Test Committee",
-                    mainSubmitter: str = "Main Submitter Country Name",
-                    coSubmitters: list[str] | None = None,
-                    topic: str = "Test Topic",
-                    preambs: list[preamb] | None = None) -> None:
-        self.committee = committee;
-        self.mainSubmitter = mainSubmitter;
+    def __init__(self, 
+                committee: str = "Test Committee",
+                mainSubmitter: str = "Main Submitter Country Name",
+                coSubmitters: list[str] | None = None,
+                topic: str = "Test Topic",
+                preambs: list[preamb] | None = None,
+                clauses: list[clause] | None = None) -> None:
+        self.committee = committee
+        self.mainSubmitter = mainSubmitter
         self.coSubmitters = coSubmitters if coSubmitters is not None else []
         self.topic = topic
         self.preambs = preambs if preambs is not None else []
-    
+        self.clauses = clauses if clauses is not None else []
